3022a9cbacaa8f684499d572bbb8dda9e60c1644
sir-goshawk <kelvin3@illinois.edu>
2019-11-22 19:28:54 -0600

    Commit 1.2 for not understanding APIs

--- app/src/main/java/com/example/genedcatalog/MainList.java
+++ app/src/main/java/com/example/genedcatalog/MainList.java
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainList extends AppCompatActivity {
 
-
     private static final String TAG = "MainList";
 
     protected void onCreate(final Bundle savedInstanceState) {
@@ -17,15 +16,13 @@ public class MainList extends AppCompatActivity {
         // The "super" call is required for all activities
         super.onCreate(savedInstanceState);
 
-
         TextView mainTitle = findViewById(R.id.MainTitle);
         mainTitle.setText("Welcome to Course Catalog");
 
         TextView filterName = findViewById(R.id.FilterName);
         filterName.setText("Filter Options: ");
-
     }
-    public MainList() {
-//        TextView mainTitle = findView;
+
+    private void testAPI() {
     }
 }
