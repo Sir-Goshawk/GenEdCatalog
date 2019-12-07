e097fbc91d763230963cfa9f256211093228c827
kelvin3 <kelvin3@illinois.edu>
2019-12-07 14:32:55 -0600

    Commit 2.3 12/7

--- app/src/main/java/com/example/genedcatalog/MainList.java
+++ app/src/main/java/com/example/genedcatalog/MainList.java
@@ -16,6 +16,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -24,11 +25,21 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.XML;
 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
 public class MainList extends AppCompatActivity {
 
     private static final String TAG = "MainList";
 
     private LinearLayout courseLists;
+
+    private JSONObject toReturn;
+
+    private String urlBASEstart = "http://courses.illinois.edu/cisapp/explorer/schedule/2020/spring/";
+
+    private String urlBASEend = ".xml";
+
 
     /**
      * What shows up when the app first opens
@@ -50,37 +61,28 @@ public class MainList extends AppCompatActivity {
         View courseChunk = getLayoutInflater().inflate(R.layout.chunk_course, courseLists, false);
 
         //USED TO MAKE SURE UI WORKED: Testing to see if a course chunk will add to the list
-//        addChunkCourse(courseChunk,"History Class", "HIST141", "HP \nWW",
-//                "THis is a description", 3);
-
-        //Instantiate the RequestQueue--gets the Course Explorer API
+//         requestAPI("http://courses.illinois.edu/cisapp/explorer/schedule/2020.xml");
+//         requestAPI("http://courses.illinois.edu/cisapi/gened.xml");
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://courses.illinois.edu/cisapp/explorer/schedule.xml";
 
-        Log.d("mine", "Message is called");
         //Request a string response from the provided URL
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        JSONObject res = new JSONObject();
-                        JSONObject resFall2020;
-                        try {
-                            res = XML.toJSONObject(response);
-                            resFall2020 = res.getJSONObject("ns2:calendarYear").getJSONObject("terms").getJSONArray("term").getJSONObject(2).getJSONObject("href");
-                        } catch (JSONException e) {
-                            Log.d("mine", e +"");
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("mine", "the error is "+error);
-            }
-        });
+//        String url = "http://courses.illinois.edu/cisapp/explorer/schedule/2012/spring/CS/125.xml";
+        String url = "https://courses.illinois.edu/cisapi/term/2011.xml";
+//        String url = "http://courses.illinois.edu/cisapi/catalog/2012/spring?mode=summary";
 
-        // Add the request to the RequestQueue.
+        ArrayList<String> subject = new ArrayList<>();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
+            try {
+                JSONObject fall2020 = XML.toJSONObject(response);
+//                Log.d("mine", ""+fall2020.getJSONObject("ns2:course").getJSONObject("sections").getJSONArray("section").getJSONObject(1).get("href"));
+            } catch (JSONException e) {
+                Log.d("mine", url + e);
+            }
+        }, error -> {
+            int  statusCode = error.networkResponse.statusCode;
+            Log.d("mine", url + " " + error + "code " + statusCode);});
         queue.add(stringRequest);
+//        requestAPI(url, new JSONObject());
     }
 
     //FOLLOWING TWO FUNCTIONS TESTED TO MAKE SURE UI WORKED (kinda...)
@@ -120,5 +122,47 @@ public class MainList extends AppCompatActivity {
         courseLists.addView(courseChunk);
     }
 
+    private String removeBackSlash(String toEdit) {
+        for (int i = 0; i < toEdit.length(); i++) {
+            if (toEdit.charAt(i) == '\\') {
+                toEdit = toEdit.substring(0, i) + toEdit.substring(i + 1);
+            }
+        }
+//        Log.d("mine", "remove is"  +toEdit);
+        return toEdit;
+    }
 
+    private void requestAPI(final String url, final JSONObject lastResponse) {
+        if (lastResponse == null) {
+            return;
+        }
+        try {
+            if (lastResponse.get("ns2:course") != null) {
+                courseJSONObject(lastResponse);
+            }
+        } catch (JSONException e) {
+            Log.d("mine", "error " + e);
+        }
+//        RequestFuture<String> test = RequestFuture.newFuture();
+        //Instantiate the RequestQueue--gets the Course Explorer API
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        //Request a string response from the provided URL
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
+            try {
+                JSONObject fall2020 = XML.toJSONObject(response);
+            Log.d("mine", "resonse " + fall2020.toString(2));
+            } catch (JSONException e) {
+                Log.d("mine", "error " + e);
+            }
+        }, error -> { });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        Log.d("mine", "returned " +toReturn);
+        return;
+    }
+
+    private void courseJSONObject(final JSONObject courseJSON) {
+        Log.d("mine", "called " + courseJSON);
+    }
 }
